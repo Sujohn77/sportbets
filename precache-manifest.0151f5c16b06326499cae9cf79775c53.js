3b1e94e103a9de4f75c0ffe37885b0eeb720fcd4
Sujohn78 <stonebo0sh56@gmail.com>
2019-09-15 15:35:33 +0300

    Updates

---
+++ precache-manifest.0151f5c16b06326499cae9cf79775c53.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5ee9798612944abf96b86090378d57",
+    "url": "/sportbets/index.html"
+  },
+  {
+    "revision": "38bdaf4a30cef74a122a",
+    "url": "/sportbets/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e2bcf1e2000df5a37b7d",
+    "url": "/sportbets/static/css/main.901e3209.chunk.css"
+  },
+  {
+    "revision": "38bdaf4a30cef74a122a",
+    "url": "/sportbets/static/js/2.fff5e64c.chunk.js"
+  },
+  {
+    "revision": "e2bcf1e2000df5a37b7d",
+    "url": "/sportbets/static/js/main.959edb5f.chunk.js"
+  },
+  {
+    "revision": "3fc3f63f860e4d5fc131",
+    "url": "/sportbets/static/js/runtime~main.57ec881c.js"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/sportbets/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "efe41bc54ff9ada55c7863345e5c3780",
+    "url": "/sportbets/static/media/logo.efe41bc5.svg"
+  },
+  {
+    "revision": "d0e19a25984d8b384a56d17373b49d80",
+    "url": "/sportbets/static/media/montserrat.d0e19a25.ttf"
+  },
+  {
+    "revision": "0e11930b17b60f5bb258961d1c2c5634",
+    "url": "/sportbets/static/media/telegram.0e11930b.svg"
+  },
+  {
+    "revision": "d0338f26eb2284579e5913e31a2ed23e",
+    "url": "/sportbets/static/media/user-default.d0338f26.svg"
+  }
+]);
